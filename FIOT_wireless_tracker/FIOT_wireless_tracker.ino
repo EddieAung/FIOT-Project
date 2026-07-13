17435849ea77b653ffa8a5b6bc7913942cbd099d
samuel <153573680+1w23www@users.noreply.github.com>
2026-07-13 10:10:03 +0800

    Update FIOT_wireless_tracker.ino

--- FIOT_wireless_tracker/FIOT_wireless_tracker.ino
+++ FIOT_wireless_tracker/FIOT_wireless_tracker.ino
@@ -1,7 +1,22 @@
+//libs
 #include
 #include
 #include
 #include
+//defs to change the names if needed
+#define 3-axis sensor ADXL345: SDA A4
+#define 3-axis sensor ADXL345: SCL A5
+#define SD card: MISO 12
+#define SD card: MOSI 11
+#define SD card: SCK 13
+#define SD card: CS 10
+#define Buzzer 9
+#define LED 8
+#define DIP switches (2 only) 7-6
+#define Soft serial Tx (NEO-6M) 5
+#define Soft serial Rx (NEO-6M) 4
+#define Soft serial Tx (ESP01) 3
+#define Soft serial Rx (ESP01) 2
 
 void setup() {
   // put your setup code here, to run once:
