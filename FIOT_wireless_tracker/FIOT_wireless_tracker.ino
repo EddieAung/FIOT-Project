88cc6845a96f18159c1381e8b1e5ce485983054b
samuel <sammlee256@gmail.com>
2026-07-17 11:45:11 +0800

    Update FIOT_wireless_tracker.ino

--- FIOT_wireless_tracker/FIOT_wireless_tracker.ino
+++ FIOT_wireless_tracker/FIOT_wireless_tracker.ino
@@ -1,8 +1,8 @@
 //libs
 #include <SoftwareSerial.h>
 #include <wire.h>
-#include
-#include
+#include <SPI.h>
+#include <SD.h>
 
 //defs 
 #define threeaxisSDA A4 //SDA
@@ -53,3 +53,37 @@ void loop()
     Serial.write(GPS_Serial.read()); // Output the raw GPS data to the serial monitor
   }
 }
+
+
+//sd card sample code
+
+const int chipSelect = 10;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // wait for Serial Monitor to open
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  int sensor = analogRead(A0);
+  String dataString = String(sensor);
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE); // use 8.3 file name
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    Serial.println(dataString); // echo on Serial Monitor
+  }
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+
+  delay(5000);
+}
+
