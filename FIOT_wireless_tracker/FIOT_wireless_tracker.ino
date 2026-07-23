a5ae5fc2aa765cc7508f59bf5722636db643d092
samuel <sammlee256@gmail.com>
2026-07-23 15:37:17 +0800

    Update FIOT_wireless_tracker.ino

--- FIOT_wireless_tracker/FIOT_wireless_tracker.ino
+++ FIOT_wireless_tracker/FIOT_wireless_tracker.ino
@@ -34,56 +34,28 @@ void setup() {
 }
 
 void loop() {
-  int alarm=0;//to change
+
+int alarm=0;//to change
+
 //ALARM MODE
 while(x==1)
-{digitalWrite(LED,HIGH);
-//digitalWrite(9,HIGH);
+{
+digitalWrite(LED,HIGH);
+digitalWrite(9,HIGH);
 delay(500);
 digitalWrite(LED,LOW);
-//digitalWrite(9,LOW);
+digitalWrite(9,LOW);
 delay(500);
 }
-}
-//to look at files and check for sample code for the various sensors. Under the file name Arudiuno sample code for IO devices 
-// GPS sample code
-void loop()
-{
-  while (GPS_Serial.available() > 0) {
+
+//GPS
+while (GPS_Serial.available() > 0)
+    {
     Serial.write(GPS_Serial.read()); // Output the raw GPS data to the serial monitor
-  }
+    }
 }
 
+//to look at files and check for sample code for the various sensors. Under the file name Arudiuno sample code for IO devices 
 
-//sd card sample code
 
-const int chipSelect = 10;
-
-void setup() {
-  Serial.begin(9600);
-  while (!Serial); // wait for Serial Monitor to open
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    return;
-  }
-  Serial.println("card initialized.");
-}
-
-void loop() {
-  int sensor = analogRead(A0);
-  String dataString = String(sensor);
-
-  File dataFile = SD.open("datalog.txt", FILE_WRITE); // use 8.3 file name
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    Serial.println(dataString); // echo on Serial Monitor
-  }
-  else {
-    Serial.println("error opening datalog.txt");
-  }
-
-  delay(5000);
-}
 
